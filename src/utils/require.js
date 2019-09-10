114625b54fd59d598f6122f83113e0cea577af71
wdd <1617022184@qq.com>
2019-09-10 11:39:31 +0800

    token过期处理

--- src/utils/require.js
+++ src/utils/require.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import jsonBigInt from 'json-bigint'
 import store from '@/store'
-// import { localStorage } from '@/utils/localStorage.js'
+import router from '@/router'
 
 // 为适应以后的多服务器请求，将axios实例化
 let instance = axios.create({
@@ -28,9 +28,35 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(function (response) {
   // 对返回的数据结构简化操作
   return response.data.data || response.data
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error)
+}, async function (error) {
+  // 401表示token过期或未登录
+  if (error.response.status === 401) {
+    // 检测vuex是否存在token，如果存在说明是过期则用refresh_token获取最新的token
+    if (store.state.token.token) {
+      const refreshToken = store.state.token.refresh_token
+      // 携带refresh_token去获取新的可用token
+      try {
+        const result = await axios({
+          method: 'put',
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${refreshToken}`
+          }
+        })
+        const newToken = result.data.data.token
+        // 将请求回来的新token存储在vuex
+        store.commit('setUserToken', {
+          token: newToken,
+          refresh_token: refreshToken
+        })
+        // 重新发送请求
+        return instance(error.config)
+      } catch (error) {
+        router.push({ path: '/login', query: { direct: router.currentRoute.fullPath } })
+      }
+    }
+  }
+  // return Promise.reject(error)
 })
 
 // 大数字处理（原因是在存储数据时以自增Number最为唯一标识，当数据量达到一定量级时这个数字就超出了Number的最大安全整数，
